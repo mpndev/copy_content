f0453738cbaed446d36c93339993df5ec92a49a1
martin <martin.nikolov@ffwagency.com>
2019-12-06 18:02:51 +0200

    Version alpha is ready.

--- src/Controller/CopyContentController.php
+++ src/Controller/CopyContentController.php
@@ -19,9 +19,9 @@ class CopyContentController extends ControllerBase {
       $path = \Drupal::request()->query->get('path');
       $params = Url::fromUri("internal:" . $path)->getRouteParameters();
       $entity_type = key($params);
-      /** @var \Drupal\node\Entity\Node $node */
       $node = Node::load($params[$entity_type]);
-      $this->handleEntitiesOnExport($node, $content);
+      $this->handleDispatchExport($node, $content, (new \ReflectionClass($node))->getName(), $node->bundle());
+      $this->removeBadFields($content);
     }
     catch (\Throwable $e) {
       return $this->sendError($e->getMessage());
@@ -31,47 +31,50 @@ class CopyContentController extends ControllerBase {
   }
 
   /**
-   * @param $entity
+   * @param \Drupal\Core\Entity\EntityInterface|Node $entity
    * @param $content
+   * @param $full_classname
+   * @param $bundle
    *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \ReflectionException
+   * @throws \Drupal\Core\Entity\EntityMalformedException
    */
-  private function handleEntitiesOnExport($entity, &$content) {
-    $with_paragraphs = \Drupal::request()->query->get('with_paragraphs');
-    $with_images = \Drupal::request()->query->get('with_images');
+  private function handleDispatchExport($entity, &$content, $full_classname, $bundle) {
+    $with_field_reference_revision = \Drupal::request()->query->get('with_field_reference_revision');
+    $with_field_reference = \Drupal::request()->query->get('with_field_reference');
 
-    foreach ($entity->toArray() as $key => $field) {
-      if (!$entity->get($key)->isEmpty()) {
-        $entity_field_values = $entity->get($key)->getValue();
-        foreach ($entity_field_values as $delta => $entity_field_value) {
-          foreach ($entity_field_value as $value_name => $value) {
-            $field_definition = $entity->get($key)->getFieldDefinition();
-            if ($field_definition->getType() === 'entity_reference_revisions') {
-              if ($with_paragraphs) {
-                $target_type = $field_definition->getItemDefinition()->getSetting('target_type');
-                $target = \Drupal::entityTypeManager()->getStorage($target_type)->load($entity_field_value['target_id']);
-                $this->handleEntitiesOnExport($target, $content[$key][$delta]);
-              }
-              else {
-                unset($content[$key]);
-              }
-            }
-            else {
-              $content[$key][$value_name] = $value;
-              if ($field_definition->getType() === 'image') {
-                if ($with_images) {
-                  $content[$key]['url'] = file_create_url($entity->get($key)->entity->getFileUri());
-                }
-                else {
-                  unset($content[$key]);
-                }
-              }
-            }
+    $content['full_classname'] = ['value' => $full_classname];
+    $content['bundle'] = ['value' => $bundle];
+    foreach ($entity->toArray() as $field_name => $entity_field) {
+      if (method_exists($entity->get($field_name), 'getFieldDefinition')) {
+        /** @var \Drupal\Core\Field\FieldDefinition $field_definition */
+        $field_definition = $entity->get($field_name)->getFieldDefinition();
+        $field_type = $field_definition->getType();
+        $field_value = $entity->get($field_name)->getValue();
+
+        if ($field_type === 'entity_reference_revisions' && $with_field_reference_revision) {
+          /** @var \Drupal\Core\Entity\EntityInterface $referenced_entity */
+          foreach ($entity->get($field_name)->referencedEntities() as $referenced_entity_index => $referenced_entity) {
+            $this->handleDispatchExport($referenced_entity, $content[$field_name][], (new \ReflectionClass($referenced_entity))->getName(), $referenced_entity->bundle());
+          }
+        }
+        else if ($field_type === 'entity_reference' && $with_field_reference) {
+          /** @var \Drupal\Core\Entity\EntityInterface $referenced_entity */
+          foreach ($entity->get($field_name)->referencedEntities() as $referenced_entity_index => $referenced_entity) {
+            $this->handleDispatchExport($referenced_entity, $content[$field_name][], (new \ReflectionClass($referenced_entity))->getName(), $referenced_entity->bundle());
+          }
+        }
+        else if (! ($field_type === 'entity_reference_revisions') && ! ($field_type === 'entity_reference')) {
+          foreach ($field_value as $value) {
+            $content[$field_name] = $value;
+          }
+          if ($field_type === 'image' && method_exists($entity->get($field_name)->entity, 'getFileUri')) {
+            $content[$field_name]['url'] = $entity->get($field_name)->entity->url();
           }
         }
       }
     }
+    unset($content['type']);
   }
 
   /**
@@ -89,14 +92,14 @@ class CopyContentController extends ControllerBase {
    */
   public function import() {
     $path = \Drupal::request()->query->get('path');
-    $with_paragraphs = \Drupal::request()->query->get('with_paragraphs');
-    $with_images = \Drupal::request()->query->get('with_images');
+    $with_field_reference_revision = \Drupal::request()->query->get('with_field_reference_revision');
+    $with_field_reference = \Drupal::request()->query->get('with_field_reference');
 
     $url_parsed = parse_url($path);
     $export_path = '';
     if (!empty($url_parsed['path'])) {
       if (!empty($url_parsed['scheme']) && !empty($url_parsed['host'])) {
-        $export_path = $url_parsed['scheme'] . '://' . $url_parsed['host'] . '/admin/content/export?path=' . $url_parsed['path'] . '&with_paragraphs=' . $with_paragraphs . '&with_images=' . $with_images;
+        $export_path = $url_parsed['scheme'] . '://' . $url_parsed['host'] . '/admin/content/export?path=' . $url_parsed['path'] . '&with_field_reference_revision=' . $with_field_reference_revision . '&with_field_reference=' . $with_field_reference;
       }
       else {
         $host = \Drupal::request()->getSchemeAndHttpHost();
@@ -110,11 +113,10 @@ class CopyContentController extends ControllerBase {
       /** @var \GuzzleHttp\Psr7\Response $request */
       $request = $client->get($export_path);
       $response = json_decode($request->getBody()->getContents(), TRUE);
-      $this->removeBadFields($response);
-      $node = Node::create(['type' => $response['type']['target_id']]);
-      $this->handleEntitiesOnImport($node, $response);
+      $entity = $response['full_classname']['value']::create(['type' => $response['bundle']['value']]);
+      $this->handleEntitiesOnImport($entity, $response);
 
-      return $this->entityFormBuilder()->getForm($node);
+      return $this->entityFormBuilder()->getForm($entity);
     }
     catch (\Throwable $e) {
       return ['#markup' => t('Failed to reach the target url.')];
@@ -122,29 +124,77 @@ class CopyContentController extends ControllerBase {
   }
 
   /**
-   * @param $entity
+   * @param \Drupal\Core\Entity\EntityInterface|Node $entity
    * @param $fields
    *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    * @throws \Drupal\Core\Entity\EntityStorageException
    */
   private function handleEntitiesOnImport(&$entity, $fields) {
+    unset($fields['full_classname']);
+    unset($fields['bundle']);
+
     foreach ($fields as $key => $field) {
-      if ($this->isAssoc($field)) {
+      if ($this->isSimpleField($field)) {
         $field = $this->fieldIsImage($field) ? $this->handleImage($field) : $field;
         $entity->set($key, $field);
       }
-      else if (is_array($field)) {
-        foreach ($field as $key2 => $inner_field_data) {
-          $target_id = $inner_field_data['type']['target_id'];
-          $target_type = $entity->getFieldDefinition($key)->getSetting('target_type');
-          $namespace_entities = 'Drupal\paragraphs\Entity\\';
-          $inner_entity = ($namespace_entities . ucfirst($target_type))::create(['type' => $target_id]);
-
-          $this->handleEntitiesOnImport($inner_entity, $inner_field_data);
-          $entity->get($key)->appendItem($inner_entity);
+      else if ($this->isNumericArray($field)) {
+        foreach ($field as $value) {
+          $this->handleEntityCreation($entity, $key, $value);
         }
       }
+      else if ($this->isEntity($field)) {
+        $this->handleEntityCreation($entity, $key, $field);
+      }
     }
+  }
+
+  /**
+   * @param \Drupal\Core\Entity\EntityInterface|Node $entity
+   * @param $field_name
+   * @param $fields
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function handleEntityCreation(&$entity, $field_name, $fields) {
+    $creation_params = ['type' => $fields['bundle']['value']];
+    if ($fields['full_classname']['value'] === 'Drupal\taxonomy\Entity\Term') {
+      $creation_params = ['vid' => 'tags'];
+    }
+    $inner_entity = $fields['full_classname']['value']::create($creation_params);
+    $this->handleEntitiesOnImport($inner_entity, $fields);
+    $entity->get($field_name)->appendItem($inner_entity);
+  }
+
+  /**
+   * @param $field
+   *
+   * @return bool
+   */
+  protected function isSimpleField($field) {
+    return array_key_exists('value', $field) || ! empty($field['alt']);
+  }
+
+  /**
+   * @param $field
+   *
+   * @return bool
+   */
+  protected function isNumericArray($field) {
+    return ! (count(array_filter(array_keys($field), 'is_string')) > 0);
+  }
+
+  /**
+   * @param $field
+   *
+   * @return bool
+   */
+  protected function isEntity($field) {
+    return array_key_exists('full_classname', $field);
   }
 
   /**
@@ -160,29 +210,15 @@ class CopyContentController extends ControllerBase {
    * @param $field
    *
    * @return mixed
-   * @throws \Drupal\Core\Entity\EntityStorageException
    */
   public function handleImage(&$field) {
     /** @var File $file */
     $image_url = $field['url'];
     $file = system_retrieve_file($image_url, NULL, TRUE);
     if ($file) {
-      $file->setPermanent();
-      $file->save();
-      unset($field['image_url']);
       $field['target_id'] = $file->id();
     }
     return $field;
-  }
-
-  /**
-   * @param array $arr
-   *
-   * @return bool
-   */
-  private function isAssoc(array $arr) {
-      if (array() === $arr) return false;
-      return array_keys($arr) !== range(0, count($arr) - 1);
   }
 
   /**
@@ -194,7 +230,9 @@ class CopyContentController extends ControllerBase {
       'nid',
       'uid',
       'vid',
+      'tid',
       'uuid',
+      'langcode',
       'created',
       'parent_type',
       'parent_field_name',
@@ -203,17 +241,26 @@ class CopyContentController extends ControllerBase {
       'paragraph__revision_id',
       'comment',
       'field_tags',
+      'revision_timestamp',
+      'revision_uid',
+      'status',
+      'changed',
+      'promote',
+      'sticky',
+      'default_langcode',
+      'revision_default',
+      'revision_created',
+      'revision_translation_affected',
+      'path',
+      'target_id',
+      'behavior_settings',
     ];
     foreach ($data as $key => &$field) {
-      if (in_array($key, $not_allowed_fields)) {
+      if (in_array($key, $not_allowed_fields) && ! is_int($key)) {
         unset($data[$key]);
       }
       if (is_array($data[$key])) {
-        foreach ($data[$key] as $child_key => &$child) {
-          if (is_int($child_key)) {
-            $this->removeBadFields($child);
-          }
-        }
+        $this->removeBadFields($data[$key]);
       }
     }
   }
--- src/Form/CopyContentForm.php
+++ src/Form/CopyContentForm.php
@@ -41,20 +41,20 @@ class CopyContentForm extends FormBase {
       '#description' => t('Copy-paste the full URL of the page for the content that you want to copy.'),
       '#required' => TRUE,
     ];
-    $form['with_paragraphs'] = [
+    $form['field_reference_revision'] = [
       '#type' => 'checkbox',
-      '#title' => t('Paragraphs?'),
+      '#title' => t('field_reference_revision?'),
       '#attributes' => [
-        'title' => t('Do you want paragraphs references to be included? (if there are available)'),
+        'title' => t('Do you want fields from type "field_reference_revision" to be included?'),
         'checked' => 'checked'
       ],
-      '#prefix' => '<br /><h5>Including:</h5>',
+      '#prefix' => '<br /><h5>Including content for fields with:</h5>',
     ];
-    $form['with_images'] = [
+    $form['field_reference'] = [
       '#type' => 'checkbox',
-      '#title' => t('Images?'),
+      '#title' => t('field_reference?'),
       '#attributes' => [
-        'title' => t('Do you want images to be included? (if there are available)'),
+        'title' => t('Do you want fields from type "field_reference" to be included?'),
         'checked' => 'checked'
       ],
     ];
@@ -74,13 +74,12 @@ class CopyContentForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $url_target = $form_state->getValue('url');
-    $with_paragraphs = $form_state->getValue('with_paragraphs');
-    $with_images = $form_state->getValue('with_images');
-    $with_tags = $form_state->getValue('with_tags');
+    $with_field_reference_revision = $form_state->getValue('field_reference_revision');
+    $with_field_reference = $form_state->getValue('field_reference');
     $url = Url::fromRoute('copy_content.import', [
       'path' => $url_target,
-      'with_paragraphs' => $with_paragraphs,
-      'with_images' => $with_images,
+      'with_field_reference_revision' => $with_field_reference_revision,
+      'with_field_reference' => $with_field_reference,
     ]);
     $form_state->setRedirectUrl($url);
   }
